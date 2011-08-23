9b2eaeb7c343f1968eb43f18a8b881a734699ad9
Peter <el-nebuloso@gmx.de>
2011-08-23 19:03:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zoim::Application.config.secret_token = '470b82292bbdf9a6c045c29ddb61189e909268af0d9a280a030dd450bca57b0c3fe6920d2abaf7d1f1c32068faa02662201098ff1118c60c8857daa10c16120d'
